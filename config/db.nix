6e5dee0d5c5be1240e17673b300f98a08f0e47d1
l.iffland <lars.iffland@mayflower.de>
2024-11-19 10:22:03 +0100

    db plugin
    untested

---
+++ config/db.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  extraPlugins = with pkgs; [
+    vimPlugins.supermaven-nvim # AI code completion
+    vimPlugins.vim-dadbod # DB client
+    vimPlugins.vim-dadbod-completion # DB completion
+    vimPlugins.vim-dadbod-ui # DB UI
+  ];
+
+  extraConfigLua = ''
+    require("telescope").load_extension('harpoon')
+
+    require("notify").setup({
+      background_colour = "#000000",
+    })
+  '';
+}
